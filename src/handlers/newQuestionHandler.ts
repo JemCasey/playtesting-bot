7024a9d48fe416f23b4668ec9cf1b1d14d9ccad3
Jordan Brownstein <jordanzbrownstein@gmail.com>
2023-10-22 10:10:50 -0400

    added category tag format option

--- src/handlers/newQuestionHandler.ts
+++ src/handlers/newQuestionHandler.ts
@@ -3,6 +3,24 @@ import { BONUS_DIFFICULTY_REGEX, BONUS_REGEX, TOSSUP_REGEX } from "src/constants
 import KeySingleton from "src/services/keySingleton";
 import { getServerChannels, getTossupParts, removeSpoilers, saveBonus, saveTossup, shortenAnswerline } from "src/utils";
 
+const extractCategory = (metadata:string | undefined) => {
+    if (!metadata)
+        return "";
+    
+    metadata = removeSpoilers(metadata);
+    let results = metadata.match(/([A-Z]{2,3}), (.*)/);
+
+    if (results)
+        return results[2].trim();
+    
+    results = metadata.match(/(.*), ([A-Z]{2,3})/);
+
+    if (results)
+        return results[1].trim();
+    
+    return "";
+}
+
 export default async function handleNewQuestion(message:Message<boolean>) {
     const bonusMatch = message.content.match(BONUS_REGEX);
     const tossupMatch = message.content.match(TOSSUP_REGEX);
@@ -19,18 +37,18 @@ export default async function handleNewQuestion(message:Message<boolean>) {
         const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button);
 
         if (bonusMatch) {
-            const [_, __, part1, answer1, part2, answer2, part3, answer3, category] = bonusMatch;
+            const [_, __, part1, answer1, part2, answer2, part3, answer3, metadata] = bonusMatch;
             const difficulty1Match = part1.match(BONUS_DIFFICULTY_REGEX) || [];
             const difficulty2Match = part2.match(BONUS_DIFFICULTY_REGEX) || [];
             const difficulty3Match = part3.match(BONUS_DIFFICULTY_REGEX) || [];
 
-            saveBonus(message.id, message.guildId!, message.author.id, removeSpoilers(category), [
+            saveBonus(message.id, message.guildId!, message.author.id, extractCategory(metadata), [
                 { part: 1, answer: shortenAnswerline(answer1), difficulty: difficulty1Match[1] || null},
                 { part: 2, answer: shortenAnswerline(answer2), difficulty: difficulty2Match[1] || null},
                 { part: 3, answer: shortenAnswerline(answer3), difficulty: difficulty3Match[1] || null}
             ], key);
         } else if (tossupMatch) {
-            const [_, question, answer, category] = tossupMatch;
+            const [_, question, answer, metadata] = tossupMatch;
             const tossupParts = getTossupParts(question);
             const questionLength = tossupParts.reduce((a, b) => {
                 return a + b.length;
@@ -40,7 +58,7 @@ export default async function handleNewQuestion(message:Message<boolean>) {
             if (tossupParts.length <= 2)
                 return;
 
-            saveTossup(message.id, message.guildId!, message.author.id, questionLength, category ? removeSpoilers(category) : "", shortenAnswerline(answer), key);
+            saveTossup(message.id, message.guildId!, message.author.id, questionLength, extractCategory(metadata), shortenAnswerline(answer), key);
         }
 
         await message.reply({ components: [row] });
