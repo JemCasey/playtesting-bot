272a508423650c043c6e574cf213e847a3374870
Jordan Brownstein <jordanzbrownstein@gmail.com>
2023-11-24 08:40:09 -0500

    #13 Allow option for bonus part difficulty marking besides [10emh] brackets

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const TOSSUP_REGEX = /(.*)\nANSWER:(.*)(?:\n?.*?\n<(.*)>)?/;
-export const BONUS_REGEX = /(.+)\n(.+)\nANSWER:(.+)\n(.+)\nANSWER:(.+)\n(.+)\nANSWER:(.+)(?:\n?.*?\n<(.*)>)?/;
+export const BONUS_REGEX = /(.+)\n(.+)\nANSWER:(.+)\n(.+)\nANSWER:(.+)\n(.+)\nANSWER:(.+)(?:\n?.*?\n<(.*)>)?(?:\n.*([emh])\/([emh])\/([emh]).*)?/;
 export const BONUS_DIFFICULTY_REGEX = /.*\[10(?:\|\|)?(\w{1})(?:\|\|)?\].*/;
 export const SECRET_ROLE = "secret";
 
--- src/handlers/newQuestionHandler.ts
+++ src/handlers/newQuestionHandler.ts
@@ -29,15 +29,15 @@ export default async function handleNewQuestion(message:Message<boolean>) {
 
     if (playtestingChannels.find(c => c.channel_id === message.channel.id) && (bonusMatch || tossupMatch)) {
         if (bonusMatch) {
-            const [_, __, part1, answer1, part2, answer2, part3, answer3, metadata] = bonusMatch;
+            const [_, __, part1, answer1, part2, answer2, part3, answer3, metadata, difficultyPart1, difficultyPart2, difficultyPart3] = bonusMatch;
             const difficulty1Match = part1.match(BONUS_DIFFICULTY_REGEX) || [];
             const difficulty2Match = part2.match(BONUS_DIFFICULTY_REGEX) || [];
             const difficulty3Match = part3.match(BONUS_DIFFICULTY_REGEX) || [];
 
             saveBonus(message.id, message.guildId!, message.author.id, extractCategory(metadata), [
-                { part: 1, answer: shortenAnswerline(answer1), difficulty: difficulty1Match[1] || null},
-                { part: 2, answer: shortenAnswerline(answer2), difficulty: difficulty2Match[1] || null},
-                { part: 3, answer: shortenAnswerline(answer3), difficulty: difficulty3Match[1] || null}
+                { part: 1, answer: shortenAnswerline(answer1), difficulty: difficultyPart1 || difficulty1Match[1] || null},
+                { part: 2, answer: shortenAnswerline(answer2), difficulty: difficultyPart2 || difficulty2Match[1] || null},
+                { part: 3, answer: shortenAnswerline(answer3), difficulty: difficultyPart3 || difficulty3Match[1] || null}
             ], key);
         } else if (tossupMatch) {
             const [_, question, answer, metadata] = tossupMatch;
