2a548779c98dbd82c78c888bcdc6dd3a0ae947e8
ani-per_github <amp7226@psu.edu>
2024-12-10 18:26:07 -0500

    Improve config directions

--- src/handlers/configHandler.ts
+++ src/handlers/configHandler.ts
@@ -2,13 +2,12 @@ import { Message, TextChannel } from "discord.js";
 import { SECRET_ROLE } from "src/constants";
 import { saveAsyncServerChannelsFromMessage, saveBulkServerChannelsFromMessage, deleteServerChannelsCommand } from "src/utils";
 
-export default async function handleConfig(message:Message<boolean>) {
-    const msgChannel = ( await message.channel.fetch() as TextChannel );
+export default async function handleConfig(message: Message<boolean>) {
+    const msgChannel = (await message.channel.fetch() as TextChannel);
 
-    await msgChannel.send('First, configure the channels used for internal, asynchronous playtesting - where the results should be saved to a separate channel.');
-    await msgChannel.send('List these channels in the form: `#playtesting-channel/#playtesting-results-channel #playtesting-channel-2/#playtesting-results-channel-2`.');
-    await msgChannel.send('Make sure to add exactly one space between each set of playtesting and results channels. Note: Multiple playtesting channels can share a `playtesting-results-channel`.');
-    await msgChannel.send('To bypass asynchronous playtesting channels, type `#/#`.');
+    await msgChannel.send('List the channels used for **internal, asynchronous playtesting** - where the results should be saved to a separate channel.');
+    await msgChannel.send('List these channels in the form: `#playtesting-channel/#playtesting-results-channel #playtesting-channel-2/#playtesting-results-channel-2`. To bypass asynchronous playtesting channels, type `#/#`.');
+    await msgChannel.send('Make sure to add _exactly one space_ between each set of playtesting and results channels. Note: Multiple playtesting channels can share a `playtesting-results-channel`.');
 
     try {
         let filter = (m: Message<boolean>) => m.author.id === message.author.id
@@ -20,14 +19,12 @@ export default async function handleConfig(message:Message<boolean>) {
         deleteServerChannelsCommand.run(message.guild!.id);
 
         saveAsyncServerChannelsFromMessage(collected, message.guild!);
-
         await msgChannel.send('Configuration saved successfully.');
         await msgChannel.send(`If you would like question answers and player notes to be encrypted in the bot's database, please create a role called \`${SECRET_ROLE}\`.`);
 
-        await msgChannel.send('Now, list the channels used for bulk playtesting - where playtesters will use reactions to indicate their results.');
-        await msgChannel.send('List these channels in the form: `#playtesting-channel #playtesting-channel-2`.');
-        await msgChannel.send('Do not repeat any channels from asynchronous playtesting. Make sure to add exactly one space between set of playtesting channels.');
-        await msgChannel.send('To bypass bulk playtesting channels, type `#/#`.');
+        await msgChannel.send('Now, list the channels used for **bulk playtesting** - where playtesters will use reactions to indicate their results.');
+        await msgChannel.send('List these channels in the form: `#playtesting-channel #playtesting-channel-2`. To bypass bulk playtesting channels, type `#/#`.');
+        await msgChannel.send('Do not repeat any channels from asynchronous playtesting. Make sure to add _exactly one space_ between set of playtesting channels.');
 
         try {
             let filter = (m: Message<boolean>) => m.author.id === message.author.id
