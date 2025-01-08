2a37ba7b9b70829f4d38571f7957fd97d623f38b
ani-per <amp7226@psu.edu>
2025-01-07 23:04:04 -0500

    Catch deleted messages error in handling for bulk playtesting

--- src/handlers/bulkQuestionHandler.ts
+++ src/handlers/bulkQuestionHandler.ts
@@ -16,70 +16,78 @@ export default async function handleTally(serverId: string, packetName: string, 
         let tallyReply = await message.reply(`Tallying reacts for ${packetBulkQuestions.length} questions in packet ${packetName} ...`);
         let talliedQuestions = 0;
         for await (const bulkQuestion of packetBulkQuestions) {
+            talliedQuestions++;
             let echoChannelId = getServerChannels(serverId).find(c => (c.channel_type === 3))?.channel_id;
             if (echoChannelId) {
                 let echoChannel = client.channels.cache.get(echoChannelId) as TextChannel;
-                let echoMessage = await echoChannel.messages.fetch(bulkQuestion.echo_id);
+                try {
+                    let echoMessage = await echoChannel.messages.fetch(bulkQuestion.echo_id);
 
-                let questionChannel = client.channels.cache.get(bulkQuestion.channel_id) as TextChannel;
-                questionChannel.messages.cache.delete(bulkQuestion.question_id);
-                let questionMessage = await questionChannel.messages.fetch(bulkQuestion.question_id);
+                    let questionChannel = client.channels.cache.get(bulkQuestion.channel_id) as TextChannel;
+                    questionChannel.messages.cache.delete(bulkQuestion.question_id);
+                    try {
+                        let questionMessage = await questionChannel.messages.fetch(bulkQuestion.question_id);
 
-                if (echoMessage && questionMessage) {
-                    let reacts: string[] = [];
-                    if (bulkQuestion.question_type === "B") {
-                        reacts = [...reacts, "bonus_E", "bonus_M", "bonus_H", "bonus_0"];
-                    } else {
-                        if (questionMessage.content.includes("\(\*\)")) {
-                            reacts = [...reacts, "tossup_15"];
+                        if (echoMessage && questionMessage) {
+                            let reacts: string[] = [];
+                            if (bulkQuestion.question_type === "B") {
+                                reacts = [...reacts, "bonus_E", "bonus_M", "bonus_H", "bonus_0"];
+                            } else {
+                                if (questionMessage.content.includes("\(\*\)")) {
+                                    reacts = [...reacts, "tossup_15"];
+                                }
+                                reacts = [
+                                    ...reacts,
+                                    "tossup_10", "tossup_0",
+                                    "tossup_DNC",
+                                    "tossup_neg5",
+                                    // "tossup_FTP",
+                                ];
+                            }
+                            let react_emoji = await getEmojiList(reacts);
+
+                            let reactCounts: userReact[] = [];
+                            for await (const [_, react] of questionMessage.reactions.cache?.filter(react => react_emoji.includes(react.emoji.toString()) && react.count)) {
+                                let thisReactUserCollection = await react.users.fetch();
+                                let reactUsers = thisReactUserCollection.map(u => Number(u.id));
+                                if (reactUsers) {
+                                    reactCounts.push({
+                                        users: reactUsers,
+                                        emoji: react.emoji.toString(),
+                                        count: react.count - 1
+                                    });
+                                }
+                            }
+
+                            if (reactCounts.some(userReact => userReact.count > 0)) {
+                                let answer_emoji = (await getEmojiList(["answer"]))[0];
+                                let newEcho = "### [" +
+                                (bulkQuestion.question_type === "B" ? "Bonus " : "Tossup ") +
+                                (thisServerSetting?.packet_name ? thisServerSetting?.packet_name + "." : "") +
+                                (bulkQuestion.question_number ? bulkQuestion.question_number : "") + " - " +
+                                bulkQuestion.category +
+                                "](" + questionMessage.url + ")" + "\n" +
+                                "* " + ((answer_emoji + " ") || "") +
+                                `||${bulkQuestion.answers}||`;
+
+                                let play_count_emoji = await getEmojiList(["play_count"]);
+                                let reactedUsers = [... new Set(reactCounts.map(userReact => [...userReact.users]).flat().filter(u => u != Number(client.user?.id)))];
+                                let playCount = reactedUsers.length > 0 ? reactedUsers.length : 1;
+                                newEcho += `\n* **${playCount}** × ${play_count_emoji} \t`;
+                                newEcho += reactCounts.map(userReact =>
+                                    `**${userReact.count}** × ${userReact.emoji} (${formatPercent(userReact.count/playCount)})`
+                                ).join("\t");
+
+                                echoMessage.edit(newEcho);
+                            }
                         }
-                        reacts = [
-                            ...reacts,
-                            "tossup_10", "tossup_0",
-                            "tossup_DNC",
-                            "tossup_neg5",
-                            // "tossup_FTP",
-                        ];
+                    } catch {
+                        console.log(`Question message (ID ${bulkQuestion.question_id}) for question ${bulkQuestion.question_number} in packet ${packetName} not found.`);
                     }
-                    let react_emoji = await getEmojiList(reacts);
-
-                    let reactCounts: userReact[] = [];
-                    for await (const [_, react] of questionMessage.reactions.cache?.filter(react => react_emoji.includes(react.emoji.toString()) && react.count)) {
-                        let thisReactUserCollection = await react.users.fetch();
-                        let reactUsers = thisReactUserCollection.map(u => Number(u.id));
-                        if (reactUsers) {
-                            reactCounts.push({
-                                users: reactUsers,
-                                emoji: react.emoji.toString(),
-                                count: react.count - 1
-                            });
-                        }
-                    }
-
-                    if (reactCounts.some(userReact => userReact.count > 0)) {
-                        let answer_emoji = (await getEmojiList(["answer"]))[0];
-                        let newEcho = "### [" +
-                        (bulkQuestion.question_type === "B" ? "Bonus " : "Tossup ") +
-                        (thisServerSetting?.packet_name ? thisServerSetting?.packet_name + "." : "") +
-                        (bulkQuestion.question_number ? bulkQuestion.question_number : "") + " - " +
-                        bulkQuestion.category +
-                        "](" + questionMessage.url + ")" + "\n" +
-                        "* " + ((answer_emoji + " ") || "") +
-                        `||${bulkQuestion.answers}||`;
-
-                        let play_count_emoji = await getEmojiList(["play_count"]);
-                        let reactedUsers = [... new Set(reactCounts.map(userReact => [...userReact.users]).flat().filter(u => u != Number(client.user?.id)))];
-                        let playCount = reactedUsers.length > 0 ? reactedUsers.length : 1;
-                        newEcho += `\n* **${playCount}** × ${play_count_emoji} \t`;
-                        newEcho += reactCounts.map(userReact =>
-                            `**${userReact.count}** × ${userReact.emoji} (${formatPercent(userReact.count/playCount)})`
-                        ).join("\t");
-
-                        echoMessage.edit(newEcho);
-                    }
+                } catch {
+                    console.log(`Echo message (ID ${bulkQuestion.echo_id}) for question ${bulkQuestion.question_number} in packet ${packetName} not found.`);
                 }
             }
-            talliedQuestions++;
             await tallyReply.edit(`Tallied reacts for ${talliedQuestions} of ${packetBulkQuestions.length} questions in packet ${packetName}.`);
         }
         await tallyReply.edit(`Tallied reacts for ${packetBulkQuestions.length} questions in packet ${packetName}.`);
